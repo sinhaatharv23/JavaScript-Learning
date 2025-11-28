f63b64b31265b6b350b0f8735e886c6de5d3ed52
Atharva <sinhaatharva688@gmail.com>
2025-11-29 01:24:25 +0530

    cmmitted

---
+++ tutorial-code/day_1/lec_12_Number_and_Math.js
@@ -0,0 +1,10 @@
+const score = 100;
+
+//we can dedicatedly define a variable as number using Number() function
+const balance = new Number(500);
+console.log(score);
+console.log(typeof score); //number
+console.log(balance);
+console.log(typeof balance); //object
+console.log(balance.toString());
+console.log(typeof balance); //string
