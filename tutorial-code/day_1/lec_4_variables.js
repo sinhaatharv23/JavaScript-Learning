0ce3c1434c8147c0fe3d4ad29791fbd54cf89825
Atharva <sinhaatharva688@gmail.com>
2025-11-28 18:53:52 +0530

    Name changed

---
+++ tutorial-code/day_1/lec_4_variables.js
@@ -0,0 +1,9 @@
+const accountId = 123456; //
+let accountEmail = "sinhaatharv39@gmail.com";
+var accountPassword = "myStrongPassword123";
+accountCity = "Jaipur";
+//This var, let and const are used to declare variables in JavaScript.
+//They are basically stored in the memory of the computer.
+//1. const : The value of a variable declared with const cannot be changed later in the code. It is a constant.
+//accountId = 2; This will give an error because we cannot change the value of a const variable.
+console.log(accountId);
