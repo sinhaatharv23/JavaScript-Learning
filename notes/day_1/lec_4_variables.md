cb6c6d25c5700c893345bbdab122f79d1881480a
Atharva <sinhaatharva688@gmail.com>
2025-11-28 19:12:55 +0530

    Commited and notes added

---
+++ notes/day_1/lec_4_variables.md
@@ -0,0 +1,11 @@
+This var, let and const are used to declare variables in JavaScript.
+They are basically stored in the memory of the computer.
+1.const : The value of a variable declared with const cannot be changed later in the code. It is a constant.
+2.let : The value of a variable declared with let can be changed later in the code.
+3.var : The value of a variable declared with var can also be changed later in the code.We dont use var nowadays, as they dont have scope'{}'(curly braces is the scope).
+4.Without var, let or const : If we declare a variable without var, let or const, it becomes a global variable and can be accessed from anywhere in the code.
+
+
+[NOTE:-If we dont wanna write again and again the console.log() then we can use another way to print multiple variables at once i.e. console.table();]
+
+See the tutorial-code.
--- tutorial-code/day_1/lec_4_variables.js
+++ tutorial-code/day_1/lec_4_variables.js
@@ -5,5 +5,21 @@ accountCity = "Jaipur";
 //This var, let and const are used to declare variables in JavaScript.
 //They are basically stored in the memory of the computer.
 //1. const : The value of a variable declared with const cannot be changed later in the code. It is a constant.
-//accountId = 2; This will give an error because we cannot change the value of a const variable.
+//accountId = 2; This will give an error because we cannot change the value of a const variable.:-
 console.log(accountId);
+
+accountEmail = "sinhaatharva23@gmail.com"; //This is valid because accountEmail is declared with let.
+console.log(accountEmail);
+
+accountPassword = "newStrongPassword456"; //This is valid because accountPassword is declared with var.
+console.log(accountPassword);
+accountCity = "Delhi"; //This is valid because accountCity is declared without var, let or const.
+console.log(accountCity);
+
+// If we dont wanna write again and again the console.log() then we can use another way to print multiple variables at once.
+//i.e. console.table();
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+//2. let : The value of a variable declared with let can be changed later in the code.
+//3. var : The value of a variable declared with var can also be changed later in the code.We dont use var much these days because let and const are introduced in ES6 and they provide better scope management.
+//Scope is the curly braces {} in which the variable is declared.
+//4. Without var, let or const : If we declare a variable without var, let or const, it becomes a global variable and can be accessed from anywhere in the code.
