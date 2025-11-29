e36d85411974977d656f41c070a4d0394524ff26
Atharva <sinhaatharva688@gmail.com>
2025-11-29 23:25:09 +0530

    committed

---
+++ tutorial-code/day_2/lec_17_Object_Part_2.js
@@ -0,0 +1,82 @@
+//How to declare the object using Constructor?
+const tinderUser = new Object();//Empty Object Declaration using Constructor,its a singleton object.
+console.log(tinderUser);
+
+//Or:-
+const tinderUser_2 = {};//Empty Object Declaration using Object Literal Syntax,its a non singleton object.
+console.log(tinderUser_2);
+
+tinderUser.id = "tinder001";
+tinderUser.name = "Atharva";
+tinderUser.isLoggedIn = false;
+console.log(tinderUser);
+
+// We can also add objects inside objects.
+
+const regularUser = {
+    name: "Atharva",
+    email: "sinhaatharv39@gmail.com",
+    fullName : {
+        firstName: "Atharva",
+        lastName: "Sinha"
+    }
+};
+console.log(regularUser.fullName);
+console.log(regularUser.fullName.firstName);
+
+
+//There is a concept of optional chaining too:-
+console.log(regularUser?.fullName?.firstName);//If fullName or firstName is not present, it will return undefined instead of throwing an error.
+//Optional chaining is a safe way to access nested object properties, even if an intermediate property doesn't exist.
+//It helps to avoid runtime errors when trying to access properties of undefined or null objects.
+
+
+//We can also merge the objects using Object.assign() method.
+const Obj1 = {
+    key1: "value1",
+    key2: "value2"
+};
+const Obj2 = {
+    key3: "value3",
+    key4: "value4"
+};
+
+const mergeObj =  Object.assign({},Obj1,Obj2);
+console.log(mergeObj);
+//The Object.assign() method is used to copy the values of all enumerable own properties from one or more source objects to a target object.
+//It returns the target object.
+//In the above example, Obj2 properties are copied to Obj1 and the merged object is stored in mergeObj.
+//If there are properties with the same name in both objects, the value from the source object (Obj2) will overwrite the value in the target object (Obj1).
+//We'll not use this much, instead we'll use Spread Operator to merge objects which is more efficient and cleaner.
+
+const mergedObjUsingSpread = {...Obj1, ...Obj2};
+console.log(mergedObjUsingSpread);
+
+
+//When the value comes from a database, it'll come as an array of objects.
+const users =[
+    {
+        userId: 1,
+        userName: "Atharva"
+    },
+    {
+        userId: 2,
+        userName: "Tarun"
+    },
+    {
+        userId: 3,
+        userName: "Anjali"
+    }
+];
+//If I wanna access the first user's name:
+console.log(users[0].userName);
+
+//If I wanna access keys of a particular object:- 
+console.log(Object.keys(tinderUser));
+//The Object.keys() method returns an array of a given object's own enumerable property names, iterated in the same order that a normal loop would.
+//Means we can iterate over the keys of the object using for..of loop.
+for(let key of Object.keys(tinderUser)){
+    console.log("Key is:", key);
+    console.log("Value is:", tinderUser[key]);
+}
+ 
---
+++ tutorial-code/day_2/lec_18_Object_de-structure_JSON_API.js
@@ -0,0 +1,63 @@
+//In this we'll learn about Object De-structuring and JSON API.
+const course = {
+    courseName: "JavaScript",
+    courseDuration: "3 Months",
+    courseProvider: "Udemy",
+    coursePrice: 1999
+}
+
+//To use these properties of the above object "course", we can access them using dot notation or bracket notation.
+console.log(course.courseName);
+
+//But if we have to access multiple properties of the object, then it becomes tedious to write the object name again and again.
+//To solve this problem, we can use Object De-structuring.
+//Object De-structuring Syntax
+// const {} = course;
+
+//Example of Object De-structuring:
+const {courseName} = course;
+console.log(courseName);//Now we can access courseName directly without using course.courseName
+
+// We can also rename the variable while de-structuring.
+const {courseDuration: duration} = course;
+console.log(duration);//Now we can access courseDuration using duration variable
+
+//In react and all there are some methods which return multiple values in the form of object, so we can use object de-structuring to access those values easily.
+//As in react too to access the company(as in the example below) we have to use props.company and all inside the '()' which is tedious. So instead we can use object de-structuring to access the company directly.
+// const navbar = ({company}) =>{
+
+// }
+// navbar(company = "Udemy");This is just an example to show how object de-structuring is used in react.
+
+//JSON API 
+//Jab Bhi Hame apna kaam kisi or ke sarr pr daal dena hota hai usi ko hm APIs kehte hai. APIs ka full form hota hai Application Programming Interface.
+//API ek aisa interface hai jo do applications ko ek dusre se baat karne aur data exchange karne ki suvidha pradan karta hai.
+//JSON ka full form hota hai JavaScript Object Notation. Ye ek lightweight data-interchange format hai jo humans ke liye padhna aur likhna asaan hota hai, aur machines ke liye parse karna aur generate karna bhi asaan hota hai.
+//JSON API ek aisa API hai jo data ko JSON format mein exchange karta hai.
+//Example of JSON:
+/*
+{
+    "name": "Atharva",
+    "age": 21,
+    "isStudent": true,
+    "skills": ["JavaScript", "HTML", "CSS"],
+    "price": "free"
+}
+Here keys and values both are in double quotes, means keys and values both are treated as strings.
+*/
+
+//Since we know objects now so JSON looks similar to objects but there are some differences between JSON and JavaScript objects:
+//1. In JSON, keys must be in double quotes, whereas in JavaScript objects, keys can be in double quotes, single quotes, or without quotes (if they are valid identifiers).
+//2. In JSON, string values must be in double quotes, whereas in JavaScript objects, string values can be in double quotes, single quotes, or without quotes (if they are valid identifiers).
+//3. JSON does not support functions, undefined, or comments, whereas JavaScript objects can have functions as values, undefined values, and comments.
+
+//However, to extract JSON data similar method is used like object de-structuring.
+//[NOTE:- Its not necessary that JSON data will always come in the form of objects, it can also come in the form of arrays of objects.]
+// [
+//     {},
+//     {},
+//     {}
+// ] //This is an example of JSON data in the form of an array of objects.
+
+//The most famous API is :- Random User Me API which provides random user data in JSON format for testing and prototyping purposes.
+//Tools to understand complex JSON data:- 1. JSON Formatter tool
